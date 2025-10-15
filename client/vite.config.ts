69eeac78ec9c6d48a2e4b4be2067e4d882d4b4d3
AmmaarKhan03 <ikhanammaar@gmail.com>
2025-10-15 16:42:01 -0700

    tailwind

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,10 @@
+// client/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwind()],
+    resolve: { alias: { '@': path.resolve(__dirname, 'src') } }
 })

75304c36141034a33afb1822600534b3eae2b7f3
IvanOG7777 <elderivanargueta04@gmail.com>
2025-10-15 17:01:31 -0700

    Login.tsx fixed

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,38 +1,10 @@
-<<<<<<< Updated upstream
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-=======
 import { useState } from 'react';
 import { api } from '../lib/api';
 import {Card, CardTitle, CardHeader, CardContent} from "@/components/ui/card";
 import {Button} from "@/components/ui/button"
->>>>>>> Stashed changes
 
 export default function Login() {
     return (
-<<<<<<< Updated upstream
-        <div className="min-h-[70vh] grid place-items-center">
-            <Card className="w-full max-w-sm">
-                <CardHeader>
-                    <CardTitle>Sign in</CardTitle>
-                </CardHeader>
-                <CardContent className="space-y-4">
-                    <div className="space-y-2">
-                        <Label htmlFor="email">Email</Label>
-                        <Input id="email" type="email" placeholder="you@example.com" />
-                    </div>
-                    <div className="space-y-2">
-                        <Label htmlFor="password">Password</Label>
-                        <Input id="password" type="password" />
-                    </div>
-                    <Button className="w-full">Login</Button>
-                </CardContent>
-            </Card>
-        </div>
-    )
-=======
         <>
             <Card>
                 <CardHeader>
@@ -48,6 +20,5 @@ export default function Login() {
             </Card>
         </>
     );
->>>>>>> Stashed changes
 }
 
